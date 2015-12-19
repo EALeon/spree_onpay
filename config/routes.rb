3ee2f3d17096803e1e990f705d8d65e47178d8c3
«Evgeny <caldariset@gmail.com>
2015-12-19 18:06:45 +0400

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
-Rails.application.routes.draw do
+Spree::Core::Engine.routes.draw do
   # Add your extension routes here
-  namespace :gateway do 
-    match '/onpay/:gateway_id/:order_id' => 'onpay#show',    :as => :onpay
-    match '/onpay/api' => 'onpay#api', :as => :onpay_api
+  namespace :admin do
+    namespace :gateway do 
+      get '/onpay/:gateway_id/:order_id' => 'onpay#show',    :as => :onpay
+      get '/onpay/api' => 'onpay#api', :as => :onpay_api
+    end
   end
 end
